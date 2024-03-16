70c7c29f094c00f0bec45d50c02f93302803b253
narminya <narminya@code.edu.az>
2024-03-17 00:45:41 +0400

    Initial commit

---
+++ patronus/src/test/java/com/demo/patronus/PatronusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.patronus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
