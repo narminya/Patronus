703b617f49abbd1be61d7b56eb7db880249d97ac
narminya <narminya@code.edu.az>
2024-03-25 00:57:32 +0400

    Adding TypeScript

---
+++ patronus.ui/src/enviroment.jsx
@@ -0,0 +1,9 @@
+const dev = {
+    url: {
+      KEYCLOAK_BASE_URL: "http://localhost:8080",
+      API_BASE_URL: 'http://localhost:9080',
+    }
+  }
+  
+
+  export const config = process.env.NODE_ENV === 'development' ? dev : prod
