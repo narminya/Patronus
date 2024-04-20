e84c4c95b937fa54e526c032b89b4bdb9f400f9f
narminya <narminya@code.edu.az>
2024-04-20 12:04:29 +0400

    Added storage service for s3

---
+++ patronus/src/main/java/com/demo/patronus/services/StorageService.java
@@ -0,0 +1,10 @@
+package com.demo.patronus.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+    String uploadFile(MultipartFile file);
+    byte[] downloadFile(String fileName);
+    String deleteFile(String fileName);
+}
