04ad9d4bf3b4ccaa4bf935d45093ad1a5b1ada53
narminya <narminya@code.edu.az>
2024-04-20 12:11:12 +0400

    Updated stream service

--- patronus/src/main/java/com/demo/patronus/dto/request/StreamPatchRequest.java
+++ patronus/src/main/java/com/demo/patronus/dto/request/StreamPatchRequest.java
@@ -2,7 +2,17 @@ package com.demo.patronus.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StreamPatchRequest {
 
     private String thumbnailUrl;
@@ -10,7 +20,6 @@ public class StreamPatchRequest {
     private Boolean chatEnabled;
     private Boolean chatFollowersOnly;
     private Boolean chatDelayed;
-
 
 }
 
